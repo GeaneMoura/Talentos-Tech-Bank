b295179b829bd71f65bb003a17b41ac47e01001c
GeaneMoura <geane.ambiental@gmail.com>
2021-06-29 17:40:14 -0300

    Commit inicial

---
+++ src/dia0/Main.java
@@ -0,0 +1,13 @@
+package dia0;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        para comentar
+        System.out.println("Olá mundo");
+        System.out.println(System.getProperty("java.version"));
+
+    }
+}
